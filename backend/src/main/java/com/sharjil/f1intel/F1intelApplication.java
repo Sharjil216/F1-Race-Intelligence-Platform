69612f09ff707b9db426bd33281cc5ae9fd0cd6b
Sharjil Munshi <sharjilmunshi@gmail.com>
2026-07-18 23:04:36 +0100

    ci: commit main class

---
+++ backend/src/main/java/com/sharjil/f1intel/F1intelApplication.java
@@ -0,0 +1,13 @@
+package com.sharjil.f1intel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1intelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1intelApplication.class, args);
+	}
+
+}
