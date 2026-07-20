697a7a5cbb690eec4dbf13d3e7081c4ccdfe0bf2
Sharjil Munshi <sharjilmunshi@gmail.com>
2026-07-20 20:49:29 +0100

    refactor: fix json property fields to their correct versions

--- backend/src/main/java/com/sharjil/f1intel/domain/Stint.java
+++ backend/src/main/java/com/sharjil/f1intel/domain/Stint.java
@@ -5,8 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record Stint(@JsonProperty("stint_number") Integer stintNumber,
                     @JsonProperty("session_key") Integer sessionKey,
                     @JsonProperty("driver_number") Integer driverNumber,
-                    @JsonProperty("lap-start") Integer lapStart,
-                    @JsonProperty("lap-end") Integer lapEnd,
+                    @JsonProperty("lap_start") Integer lapStart,
+                    @JsonProperty("lap_end") Integer lapEnd,
                     @JsonProperty("compound") String compound,
                     @JsonProperty("tyre_age_at_start") Integer tyreAgeAtStart) {
 }
